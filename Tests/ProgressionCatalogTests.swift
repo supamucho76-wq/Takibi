7c81dea49b49ed9cb30f3f4a051e1f85eee447a5
supamucho76-wq <supamucho76@gmail.com>
2026-08-02 13:28:58 +0900

    Test distinct fuel silhouettes

--- Tests/ProgressionCatalogTests.swift
+++ Tests/ProgressionCatalogTests.swift
@@ -10,12 +10,16 @@ final class ProgressionCatalogTests: XCTestCase {
 
     func testCrossingMilestonesReturnsEveryNewReward() {
         let rewards = ProgressionCatalog.newlyUnlocked(from: 900, to: 2_100)
-        XCTAssertEqual(Set(rewards.map(\.name)), Set(["琥珀火", "月影の森", "樫の薪"]))
+        XCTAssertEqual(Set(rewards.map(\.name)), Set(["琥珀火", "月影の森", "樫炭"]))
     }
 
     func testNextRewardPointsBeyondCurrentProgress() {
         let reward = ProgressionCatalog.nextReward(after: 1_500)
-        XCTAssertEqual(reward?.name, "樫の薪")
+        XCTAssertEqual(reward?.name, "樫炭")
         XCTAssertEqual(reward?.unlockSteps, 2_000)
+    }
+
+    func testEveryFuelHasADifferentPhysicalSilhouette() {
+        XCTAssertEqual(Set(WoodCatalog.all.map(\.visualKind)).count, 10)
     }
 }
